39f9fcd236b3b1b9b31fad96894826ecf35dd3a8
Raphael-HdF <rdefrob@gmail.com>
2021-12-11 10:34:49 +0100

    [ADD] Model Player + Model Match

---
+++ model/match.py
@@ -0,0 +1,10 @@
+
+
+class Match:
+    def __init__(self, player_1, player_2, score_p1, score_p2, turn, tournament):
+        self.player_1 = player_1
+        self.player_2 = player_2
+        self.score_p1 = score_p1
+        self.score_p2 = score_p2
+        self.turn = turn
+        self.tournament = tournament
